92f9aba770a5209301b577ae51978084162b9424
Benineza ketsia <kbenineza@gmail.com>
2024-07-22 12:03:05 +0200

    array code

---
+++ array.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	 string name;
+	 int age;
+	 cout<<"enter your name:";
+	 cin>>name;
+	 cout<<"entern your age:";
+	 cin>>age;
+	 cout<<"Hello"<<"\t"<<name<<"\t"<<  "your age is:"<< age<<"years old";
+}
+
