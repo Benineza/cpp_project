e2b1103fe0d9483f3f403c8802e13d1f936ca9fd
Benineza ketsia <kbenineza@gmail.com>
2024-07-23 09:12:31 +0200

    Enum.cpp

---
+++ Enum.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+enum weekday{Sunday=0,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday};
+int main()
+{
+	int day;
+	cout<<"Enter the number 0-6 corresonding to get the day:";
+	cin>>day;
+	switch (day)
+	{
+		case Sunday:cout<<"Sunday"<<endl;break;
+		case Monday:cout<<"Monday"<<endl;break;
+		case Tuesday:cout<<"Tuesday"<<endl;break;
+		case Wednesday:cout<<"Sunday"<<endl;break;
+		case Thursday:cout<<"Thursday"<<endl;break;
+		case Friday:cout<<"Friday"<<endl;break;
+		case Saturday:cout<<"Saturday"<<endl;break;
+		
+	}
+}
+
