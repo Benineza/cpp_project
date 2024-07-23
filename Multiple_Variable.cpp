531abd31077fdefe917f78fbc61587fb2d856e4a
Benineza ketsia <kbenineza@gmail.com>
2024-07-23 22:16:17 +0200

    Function

---
+++ Multiple_Variable.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void myFunction(string name, string ) {
+  cout<<name<<" Refsnes. "<<int fees << " years old.";
+}
+
+int main() {
+  myFunction(Havard university,);
+  myFunction(40000);
+
+  return 0;
+}
+
