51fbc80fac25f1a5d7ce286554fef44b3c22c45d
Benineza ketsia <kbenineza@gmail.com>
2024-08-06 18:36:14 +0200

    Project_1

---
+++ Simple_Calculator.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	double product;
+	double quotient;
+	double sum;
+	double difference;
+	double number1;
+	double number2;
+	
+	cout<<"Enter first number:";
+	cin>>number1;
+	cout<<"Enter second number:";
+	cin>>number2;
+	cout<<"Sum:"<<number1+number2<<endl;
+	cout<<"difference:"<<number1-number2<<endl;
+	cout<<"product:"<<number1*number2<<endl;
+	cout<<"quotient:"<<number1/number2<<endl;
+	return 0;
+}
